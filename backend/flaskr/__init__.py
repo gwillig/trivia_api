3fd5f29a8cf7702405c77a155e9e0095bcca8b82
gwillig <gustavwillig@gmail.com>
2019-12-15 21:30:10 +0100

    FEAT: backend - add new function to show all questions in the database

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,8 +1,20 @@
+# ----------------------------------------------------------------------------#
+# Own Notes.
+"""
+Description                         | Cmd
+to login as the right user for psql | PGUSER=test PGPASSWORD=test psql -h localhost trivia
+Give all right to role              | GRANT ALL PRIVILEGES ON database test to test;
+adds temporary git\bit to path      | "c:\Program Files\Git\bin\sh.exe" --login
+
+PGUSER=test PGPASSWORD=test psql -h localhost todoapp
+"""
+# ----------------------------------------------------------------------------#
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
+import json
 
 from backend.models import setup_db, Question, Category
 
@@ -17,6 +29,15 @@ def create_app(test_config=None):
     '1.Step: config cors'
     CORS(app)
     cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        '''
+        Define access controll
+        '''
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
+        return response
 
     @app.route("/questions")
     def get_questions():
@@ -49,41 +70,66 @@ def create_app(test_config=None):
 
         )
 
-    @app.after_request
-    def after_request(response):
+    @app.route('/questions',methods=['POST'])
+    def post_question():
         '''
-        Define access controll
+        @description:
+            adds a new question to the database
         '''
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
-        return response
+        '#1.Step: Get all parameters for the ajax request'
+        # question = request.args.post('question', None, type=str)
+        # answer = request.args.post('answer', None, type=str)
+        # difficulty = request.args.post('difficulty',None,type=int)
+        # category = request.args.post('category', None, type=str)
+        data_string = request.data
+        request_dict = json.loads(data_string)
+        request_dict['id'] = None #Will be automaticlly created
 
-    '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+        '#2.Step: create a new records a the database and send response back'
+        try:
+            c1 = Question(**request_dict)
+            db.session.add(c1)
+            db.session.commit()
+            print("New question has been created")
+            print(request_dict)
+        except:
+            db.session.rollback()
+            db.session.close()
+            print('An error occur during creating of')
+            print(request_dict)
+            abort(400)
+        finally:
+            db.session.close()
+        return jsonify({
+            'success': True,
+        })
 
-    '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
+    @app.route("/categories/")
+    def get_categories():
+        '''
+        @description:
+            gets all question for a give category
+        @return:
+            all question for a given category
+        '''
+        '#1.Step: get the category id'
+        category_id = request.args.get('')
+        try:
+            categories_tuple = db.session.query(Category.id,Category.type).all()
+            categories_dict = {key:value for key,value in categories_tuple}
+        except:
+            db.session.rollback()
+            db.session.close()
+            abort(404)
+        finally:
+            db.session.close()
 
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
-
-    '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
-
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+        return jsonify(
+            {
+                'success':True,
+                'categories': categories_dict
+            }
+        )
 
     '''
   @TODO: 
@@ -95,6 +141,15 @@ def create_app(test_config=None):
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
   '''
+
+    '''
+  @TODO: 
+  Create an endpoint to DELETE question using a question ID. 
+
+  TEST: When you click the trash icon next to a question, the question will be removed.
+  This removal will persist in the database and when you refresh the page. 
+  '''
+
 
     '''
   @TODO: 
