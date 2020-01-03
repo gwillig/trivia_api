6baa1a098cb7c67d5edf469c13e227d3c87b08e0
gwillig <gustavwillig@gmail.com>
2020-01-03 21:56:12 +0100

    Change to to aline to PEP8

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,13 +1,3 @@
-# ----------------------------------------------------------------------------#
-# Own Notes.
-"""Description                         | Cmd
-to login as the right user for psql |   PGUSER=test PGPASSWORD=test psql -h localhost trivia_test
-Give all right to role              | GRANT ALL PRIVILEGES ON database test to test;
-adds temporary git\bit to path      | "c:\Program Files\Git\bin\sh.exe" --login
-
-PGUSER=test PGPASSWORD=test psql -h localhost todoapp
-"""
-# ----------------------------------------------------------------------------#
 import os
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
@@ -28,8 +18,10 @@ def create_app(test_config=None):
 
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,POST,PUT,PATCH,DELETE,OPTIONS')
         return response
 
     @app.route("/questions")
@@ -37,11 +29,13 @@ def create_app(test_config=None):
         start_page_raw = request.args.get('page', 1, type=int)
         start_page = (start_page_raw - 1) * 10
         try:
-            all_questions = db.session.query(Question).order_by(Question.id).offset(start_page).limit(
-                QUESTIONS_PER_PAGE).all()
+            all_questions = db.session.query(Question)\
+                .order_by(Question.id)\
+                .offset(start_page).limit(QUESTIONS_PER_PAGE).all()
             all_questions_list = [el.format() for el in all_questions]
-            total_questions = db.session.query(Question).order_by(Question.id).count()
-            ## Get all categories
+            total_questions = db.session.query(Question)\
+                .order_by(Question.id).count()
+            # Get all categories
             all_categories_dict = {}
             all_categories = db.session.query(Category).all()
             for el in all_categories:
@@ -71,7 +65,7 @@ def create_app(test_config=None):
         request_dict = json.loads(data_string)
         '#2.Step: create a new records a the database and send response back'
 
-        if not 'searchTerm' in request_dict.keys():
+        if 'searchTerm' not in request_dict.keys():
             try:
                 c1 = Question(**request_dict)
                 db.session.add(c1)
@@ -88,7 +82,7 @@ def create_app(test_config=None):
                 'success': True,
             })
         else:
-            ## Get all categories
+            '# Get all categories'
             all_categories_dict = {}
             all_categories = db.session.query(Category).all()
             for el in all_categories:
@@ -96,7 +90,8 @@ def create_app(test_config=None):
 
             search_term = request_dict['searchTerm']
             print(search_term)
-            baseQuery = db.session.query(Question).filter(Question.question.like(f'%{search_term}%'))
+            baseQuery = db.session.query(Question).\
+                filter(Question.question.like(f'%{search_term}%'))
             result = baseQuery.all()
             all_questions_list = [el.format() for el in result]
             total_questions = baseQuery.count()
@@ -106,7 +101,8 @@ def create_app(test_config=None):
                                        'category': '4',
                                        'difficulty': 2,
                                        'id': 1,
-                                       'question': "No hits. Please change search term"
+                                       'question': "No hits."
+                                                   " Please change search term"
                                        }]
 
             return jsonify({
@@ -151,7 +147,8 @@ def create_app(test_config=None):
         '#1.Step: get the category id'
 
         try:
-            categories_tuple = db.session.query(Category.id, Category.type).all()
+            categories_tuple = db.session.query(Category.id, Category.type)\
+                .all()
             categories_dict = {key: value for key, value in categories_tuple}
             db.session.close()
             return jsonify(
@@ -166,10 +163,13 @@ def create_app(test_config=None):
     def get_all_question_category(category_id):
         print(category_id)
         try:
-            category_type = Category.query.filter_by(id=category_id).first().format()
-            all_questions = db.session.query(Question).filter_by(category=str(category_id)).all()
+            category_type = Category.query\
+                .filter_by(id=category_id).first().format()
+            all_questions = db.session.query(Question)\
+                .filter_by(category=str(category_id)).all()
             all_questions_list = [el.format() for el in all_questions]
-            total_questions = db.session.query(Question).order_by(Question.id).count()
+            total_questions = db.session.query(Question)\
+                .order_by(Question.id).count()
             # Get all categories
             all_categories_dict = {}
             all_categories = db.session.query(Category).all()
@@ -202,20 +202,23 @@ def create_app(test_config=None):
             request_dict = json.loads(data_string)
             '#2.1.Step: All all question if all is selected'
             category_id = request_dict["quiz_category"]["id"]
-            category_typ = db.session.query(Category).filter_by(id=category_id).first().type
+            category_typ = db.session.query(Category)\
+                .filter_by(id=category_id).first().type
             previous_questions = request_dict["previous_questions"]
             if category_id == 0:
                 question = db.session.query(Question).first()
             else:
-                basequey = db.session.query(Question).filter(Question.category == str(category_id))
+                basequey = db.session.query(Question).\
+                    filter(Question.category == str(category_id))
                 for question_id in previous_questions:
                     basequey = basequey.filter(Question.id != str(question_id))
-                question: object = basequey.first()
+                question = basequey.first()
                 if basequey.count() == 0:
-                    return jsonify(dict(
-                                        success=True, question={"question": False,"success": True}
-                                        )
-                                   )
+                    return jsonify(
+                        dict(success=True,
+                             question={"question": False, "success": True}
+                             )
+                    )
         except:
             db.session.rollback()
             abort(400)
@@ -225,17 +228,21 @@ def create_app(test_config=None):
 
     @app.errorhandler(400)
     def bad_request(error):
-        return jsonify(dict(success=False, error=400, message='bad request')),400
+        return jsonify(dict(success=False, error=400,
+                            message='bad request')), 400
 
     @app.errorhandler(404)
     def resource_not_found(error):
-        return jsonify(dict(success=False, error=404, message='resource not found')),404
+        return jsonify(dict(success=False, error=404,
+                            message='resource not found')), 404
 
     @app.errorhandler(422)
     def unprocessable(error):
         return jsonify(
-            dict(success=False, error=422, message='The server understands the content type of the request entity')
-        ),422
+            dict(success=False, error=422,
+                 message='The server understands the '
+                         'content type of the request entity')
+        ), 422
 
     return app
 
--- backend/models.py
+++ backend/models.py
@@ -5,14 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-database_path = 'postgresql://test:test@localhost:15432/'+database_name
+database_path = 'postgresql://test:test@localhost:15432/' + database_name
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -21,179 +18,49 @@ def setup_db(app, database_path=database_path):
     db.create_all()
     return db
 
-'''
-Question
 
-'''
-class Question(db.Model):  
-  __tablename__ = 'questions'
+class Question(db.Model):
+    __tablename__ = 'questions'
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def update(self):
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
-
-'''
-Category
-
-'''
-class Category(db.Model):  
-  __tablename__ = 'categories'
-
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
-
-  def __init__(self, type):
-    self.type = type
-
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    def format(self):
+        return {'id': self.id,
+                'question': self.question,
+                'answer': self.answer,
+                'category': self.category,
+                'difficulty': self.difficulty
+                }
 
 
-def inject_data():
-    question_list = [
-        {'answer': 'Maya Angelou',
-                      'category': 4,
-                      'difficulty': 2,
-                      'id': 5,
-                      'question': "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?"},
-                     {'answer': 'Muhammad Ali',
-                      'category': 4,
-                      'difficulty': 1,
-                      'id': 9,
-                      'question': "What boxer's original name is Cassius Clay?"},
-                     {'answer': 'Apollo 13',
-                      'category': 5,
-                      'difficulty': 4,
-                      'id': 2,
-                      'question': 'What movie earned Tom Hanks his third straight Oscar nomination, in 1996?'},
-                     {'answer': 'Tom Cruise',
-                      'category': 5,
-                      'difficulty': 4,
-                      'id': 4,
-                      'question': 'What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?'},
-                     {'answer': 'Edward Scissorhands',
-                      'category': 5,
-                      'difficulty': 3,
-                      'id': 6,
-                      'question': 'What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?'},
-                     {'answer': 'Brazil',
-                      'category': 6,
-                      'difficulty': 3,
-                      'id': 10,
-                      'question': 'Which is the only team to play in every soccer World Cup tournament?'},
-                     {'answer': 'Uruguay',
-                      'category': 6,
-                      'difficulty': 4,
-                      'id': 11,
-                      'question': 'Which country won the first ever soccer World Cup in 1930?'},
-                     {'answer': 'George Washington Carver',
-                      'category': 4,
-                      'difficulty': 2,
-                      'id': 12,
-                      'question': 'Who invented Peanut Butter?'},
-                     {'answer': 'Lake Victoria',
-                      'category': 3,
-                      'difficulty': 2,
-                      'id': 13,
-                      'question': 'What is the largest lake in Africa?'},
-                     {'answer': 'The Palace of Versailles',
-                      'category': 3,
-                      'difficulty': 3,
-                      'id': 14,
-                      'question': 'In which royal palace would you find the Hall of Mirrors?'},
-                     {'answer': 'Agra',
-                      'category': 3,
-                      'difficulty': 2,
-                      'id': 15,
-                      'question': 'The Taj Mahal is located in which Indian city?'},
-                     {'answer': 'Escher',
-                      'category': 2,
-                      'difficulty': 1,
-                      'id': 16,
-                      'question': 'Which Dutch graphic artist–initials M C was a creator of optical illusions?'},
-                     {'answer': 'Mona Lisa',
-                      'category': 2,
-                      'difficulty': 3,
-                      'id': 17,
-                      'question': 'La Giaconda is better known as what?'},
-                     {'answer': 'One',
-                      'category': 2,
-                      'difficulty': 4,
-                      'id': 18,
-                      'question': 'How many paintings did Van Gogh sell in his lifetime?'},
-                     {'answer': 'Jackson Pollock',
-                      'category': 2,
-                      'difficulty': 2,
-                      'id': 19,
-                      'question': 'Which American artist was a pioneer of Abstract Expressionism, and a leading exponent of action painting?'},
-                     {'answer': 'The Liver',
-                      'category': 1,
-                      'difficulty': 4,
-                      'id': 20,
-                      'question': 'What is the heaviest organ in the human body?'},
-                     {'answer': 'Alexander Fleming',
-                      'category': 1,
-                      'difficulty': 3,
-                      'id': 21,
-                      'question': 'Who discovered penicillin?'},
-                     {'answer': 'Blood',
-                      'category': 1,
-                      'difficulty': 4,
-                      'id': 22,
-                      'question': 'Hematology is a branch of medicine involving the study of what?'},
-                     {'answer': 'Scarab',
-                      'category': 4,
-                      'difficulty': 4,
-                      'id': 23,
-                      'question': 'Which dung beetle was worshipped by the ancient Egyptians?'}
-                     ]
-    for el in question_list:
-        el.pop('id')
-    question_objects = [Question(**el) for el in question_list]
-    db.session.add_all(question_objects)
-    db.session.commit()
-    data_category = [{'id': 1, 'type': 'Science'},
-                     {'id': 2, 'type': 'Art'},
-                     {'id': 3, 'type': 'Geography'},
-                     {'id': 4, 'type': 'History'},
-                     {'id': 5, 'type': 'Entertainment'},
-                     {'id': 6, 'type': 'Sports'}]
-    for el in data_category:
-        el.pop('id')
-    data_object = [Category(**el) for el in data_category]
-    db.session.add_all(data_object)
-    db.session.commit()
+class Category(db.Model):
+    __tablename__ = 'categories'
 
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
 
+    def __init__(self, type):
+        self.type = type
 
+    def format(self):
+        return {'id': self.id, 'type': self.type}
